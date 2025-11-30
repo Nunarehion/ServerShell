19f4bd60708db8e71d4e0b728f447b00a1343542
Nunarehion <145133685+Nunarehion@users.noreply.github.com>
2025-11-30 19:12:35 +0300

    Update +layout.svelte

--- Svelte/WebShell/src/routes/+layout.svelte
+++ Svelte/WebShell/src/routes/+layout.svelte
@@ -50,9 +50,10 @@
         margin: 0;
         padding: 0;
         background-color: var(--light-bg);
-        width: 100vw;
+        width: 100%;
         height: 100vh;
         font-family: sans-serif;
+        box-sizing: border-box;
     }
     .layout 
         display: grid
