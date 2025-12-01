2ba8ed2693cab6f688deedca45b591a3f48647ac
Nunarehion <145133685+Nunarehion@users.noreply.github.com>
2025-12-01 23:12:56 +0300

    Update +layout.svelte

--- Svelte/WebShell/src/routes/+layout.svelte
+++ Svelte/WebShell/src/routes/+layout.svelte
@@ -49,6 +49,8 @@
         --sidebar-width: 76px
     }
       :global(body) {
+        position: fixed;
+        top: 0; left: 0;
         margin: 0;
         padding: 0;
         background-color: var(--light-bg);
