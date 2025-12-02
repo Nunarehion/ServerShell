d09a811ed0404c2996995622e1f3d05904524448
Nunarehion <145133685+Nunarehion@users.noreply.github.com>
2025-12-02 17:59:32 +0300

    Update FileEntry.svelte

--- Svelte/WebShell/src/lib/components/files/FileEntry.svelte
+++ Svelte/WebShell/src/lib/components/files/FileEntry.svelte
@@ -36,9 +36,9 @@
     
     <!-- Выпадающее содержимое -->
     <div id="myDropdown" class="dropdown-content">
-      <a href="#home">Главная</a>
-      <a href="#about">О нас</a>
-      <a href="#contact">Контакты</a>
+      <a href={downloadUrl} download={file.name}>скачать</a>
+      <a href="#about">удалить</a>
+      <a href="#contact">что-то еще</a>
     </div>
   </div>
 {/snippet}
@@ -48,15 +48,15 @@
 <!-- (HTML) -->
 
 {#if file.type === 'directory'}
-    <div class="file-entry" on:click={() => onNavigate(file)}>
+    <div class="file-entry" >
     {@render DotsButton()}
-        <span class="icon">
+        <span class="icon" on:click={() => onNavigate(file)}>
            <!--(SVG)(folder)--> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-folder-fill" viewBox="0 0 16 16"><path d="M9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.825a2 2 0 0 1-1.991-1.819l-.637-7a2 2 0 0 1 .342-1.31L.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3m-8.322.12q.322-.119.684-.12h5.396l-.707-.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981z"/></svg>
         </span>
         <span class="name">{file.name}</span>
     </div>
 {:else}
-    <a class="file-entry file-link" href={downloadUrl} download={file.name}>
+    <a class="file-entry file-link">
         {@render DotsButton()}
         <span class="icon">
            <!--(SVG)(file)--> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-fill" viewBox="0 0 16 16"><path d="M4 0h5.293A1 1 0 0 1 10 .293L13.707 4a1 1 0 0 1 .293.707V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2m5.5 1.5v2a1 1 0 0 0 1 1h2z"/></svg>
