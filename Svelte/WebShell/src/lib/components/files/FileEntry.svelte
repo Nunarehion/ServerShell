b97aabd721c4533bac039c030738547af4ca3311
Nunarehion <145133685+Nunarehion@users.noreply.github.com>
2025-12-02 12:36:12 +0300

    Update FileEntry.svelte

--- Svelte/WebShell/src/lib/components/files/FileEntry.svelte
+++ Svelte/WebShell/src/lib/components/files/FileEntry.svelte
@@ -34,7 +34,7 @@
     transition: background-color 0.2s;
     color: inherit; 
     text-decoration: none;
-    box-shadow: inset 0 0 2px white
+    box-shadow: inset 0 0 .1px white
   }
   
   .file-entry:hover {
