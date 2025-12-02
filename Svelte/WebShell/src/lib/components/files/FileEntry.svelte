44b392b0fe4148f696b4849398cab24516537e27
Nunarehion <145133685+Nunarehion@users.noreply.github.com>
2025-12-02 13:31:23 +0300

    Update FileEntry.svelte

--- Svelte/WebShell/src/lib/components/files/FileEntry.svelte
+++ Svelte/WebShell/src/lib/components/files/FileEntry.svelte
@@ -12,6 +12,9 @@
 
 {#if file.type === 'directory'}
     <div class="file-entry" on:click={() => onNavigate(file)}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots" viewBox="0 0 16 16">
+  <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3m5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3m5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3"/>
+</svg>
         <span class="icon">📁</span>
         <span class="name">{file.name}</span>
     </div>
