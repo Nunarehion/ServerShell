451850c606afaa6ef4e9888c9c3d0d731a0d69c2
Nunarehion <145133685+Nunarehion@users.noreply.github.com>
2025-12-02 15:57:21 +0300

    Update FileEntry.svelte

--- Svelte/WebShell/src/lib/components/files/FileEntry.svelte
+++ Svelte/WebShell/src/lib/components/files/FileEntry.svelte
@@ -31,8 +31,8 @@
         <span class="name">{file.name}</span>
     </div>
 {:else}
-    {@render DotsButton()}
     <a class="file-entry file-link" href={downloadUrl} download={file.name}>
+        {@render DotsButton()}
         <span class="icon">
            <!--(SVG)(file)--> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-fill" viewBox="0 0 16 16"><path d="M4 0h5.293A1 1 0 0 1 10 .293L13.707 4a1 1 0 0 1 .293.707V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2m5.5 1.5v2a1 1 0 0 0 1 1h2z"/></svg>
 </span>
