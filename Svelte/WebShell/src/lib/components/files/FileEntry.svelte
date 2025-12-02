74bc800cc3a07df32c95ca4d4cdc08c8f36f0095
Nunarehion <145133685+Nunarehion@users.noreply.github.com>
2025-12-02 15:54:47 +0300

    Update FileEntry.svelte

--- Svelte/WebShell/src/lib/components/files/FileEntry.svelte
+++ Svelte/WebShell/src/lib/components/files/FileEntry.svelte
@@ -11,9 +11,9 @@
 {#snippet DotsButton()}
   <button class="dots-button" on:click={() => alert('Кнопка нажата')}>
     <!--(SVG)(3 points)-->
-    <svg xmlns="www.w3.org" width="16" height="16" fill="currentColor" class="bi bi-three-dots" viewBox="0 0 16 16"> 
-        <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3m5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3m5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3""")/>
-    </svg>
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
+  <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0"/>
+</svg>
   </button>
 {/snippet}
 
