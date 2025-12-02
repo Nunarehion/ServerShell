4ec4b0c9d7fa85e70ea8a3f078733e9b7c1cdaad
Nunarehion <145133685+Nunarehion@users.noreply.github.com>
2025-12-02 13:38:42 +0300

    Update +page.svelte

--- Svelte/WebShell/src/routes/files/+page.svelte
+++ Svelte/WebShell/src/routes/files/+page.svelte
@@ -125,11 +125,12 @@
         <code>{currentPath}</code>
     </div>
     <div class="actions">
-        <!-- Кнопка теперь отключена, если мы в корневом каталоге -->
         <button on:click={goBack} disabled={currentPath === '/'}>
-            ←
+           <!--(SVG)--> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-90deg-left" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M1.146 4.854a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 4H12.5A2.5 2.5 0 0 1 15 6.5v8a.5.5 0 0 1-1 0v-8A1.5 1.5 0 0 0 12.5 5H2.707l3.147 3.146a.5.5 0 1 1-.708.708z"/></svg>
         </button>
-        <button on:click={() => loadDirectory(currentPath)}>↻</button>
+        <button on:click={() => loadDirectory(currentPath)}>
+        <!--(SVG)--> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-counterclockwise" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M8 3a5 5 0 1 1-4.546 2.914.5.5 0 0 0-.908-.417A6 6 0 1 0 8 2z"/><path d="M8 4.466V.534a.25.25 0 0 0-.41-.192L5.23 2.308a.25.25 0 0 0 0 .384l2.36 1.966A.25.25 0 0 0 8 4.466"/></svg>
+        </button>
 
         <div class="action-dropdown">
             <button class="primary-action">+</button>
