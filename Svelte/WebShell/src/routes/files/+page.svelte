16fdc98f0ee3c55671e7469116fb76e1b7c05e6f
Nunarehion <145133685+Nunarehion@users.noreply.github.com>
2025-12-02 23:42:45 +0300

    Update +page.svelte

--- Svelte/WebShell/src/routes/files/+page.svelte
+++ Svelte/WebShell/src/routes/files/+page.svelte
@@ -139,9 +139,9 @@
                 <!--(SVG)--> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-patch-plus" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M8 5.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V10a.5.5 0 0 1-1 0V8.5H6a.5.5 0 0 1 0-1h1.5V6a.5.5 0 0 1 .5-.5"/><path d="m10.273 2.513-.921-.944.715-.698.622.637.89-.011a2.89 2.89 0 0 1 2.924 2.924l-.01.89.636.622a2.89 2.89 0 0 1 0 4.134l-.637.622.011.89a2.89 2.89 0 0 1-2.924 2.924l-.89-.01-.622.636a2.89 2.89 0 0 1-4.134 0l-.622-.637-.89.011a2.89 2.89 0 0 1-2.924-2.924l.01-.89-.636-.622a2.89 2.89 0 0 1 0-4.134l.637-.622-.011-.89a2.89 2.89 0 0 1 2.924-2.924l.89.01.622-.636a2.89 2.89 0 0 1 4.134 0l-.715.698a1.89 1.89 0 0 0-2.704 0l-.92.944-1.32-.016a1.89 1.89 0 0 0-1.911 1.912l.016 1.318-.944.921a1.89 1.89 0 0 0 0 2.704l.944.92-.016 1.32a1.89 1.89 0 0 0 1.912 1.911l1.318-.016.921.944a1.89 1.89 0 0 0 2.704 0l.92-.944 1.32.016a1.89 1.89 0 0 0 1.911-1.912l-.016-1.318.944-.921a1.89 1.89 0 0 0 0-2.704l-.944-.92.016-1.32a1.89 1.89 0 0 0-1.912-1.911z"/></svg>
                 </button>
             <div class="dropdown-content">
-                <button on:click={createNewFolder}>Новая папка 📁</button>
+                <button on:click={createNewFolder}>Новая папка</button>
                 <input type="file" id="fileInput" style="display: none;" on:change={uploadFile} />
-                <button on:click={triggerFileInput}>Загрузить файл 📄</button>
+                <button on:click={triggerFileInput}>Загрузить файл</button>
             </div>
         </div>
     </div>
@@ -187,46 +187,6 @@
       align-items: center;
       gap: 1rem;
   }
-  .actions {
-    display: flex;
-    gap: 0.5rem;
-  }
-
-  .action-dropdown {
-    position: relative;
-    display: inline-block;
-  }
-
-  .dropdown-content {
-    display: none;
-    position: absolute;
-    right: 0;
-    background-color: #f9f9f9;
-    min-width: 160px;
-    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-    z-index: 1;
-    flex-direction: column;
-  }
-
-  .dropdown-content button {
-    color: black;
-    padding: 8px 12px;
-    text-decoration: none;
-    display: block;
-    border: none;
-    background: none;
-    text-align: left;
-    width: 100%;
-    cursor: pointer;
-  }
-
-  .dropdown-content button:hover {
-    background-color: #f1f1f1;
-  }
-
-  .action-dropdown:hover .dropdown-content {
-    display: flex;
-  }
 
   .file-list {
     display: flex;
@@ -248,4 +208,38 @@
     border: 1px solid green;
     margin-bottom: 1rem;
   }
+
+
+
+.dropdown-content {
+  position: absolute;
+  left: 0;
+  background-color: black;
+  color: white
+  min-width: max-content;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.3);
+  z-index: 1;
+  
+  text-transform: uppercase;
+  background: var(--dark-bg);
+  border-radius: 8px;
+  
+}
+.dropdown-content {
+   display: none;
+}
+.dropdown-content.show {
+   display: grid; 
+}
+
+.dropdown-content button {
+  color: white;
+  padding: 1rem 1.25rem;
+  text-decoration: none;
+  display: block;
+}
+
+.dropdown-content button:hover {
+  background-color: var(--clr-hover);
+}
 </style>
