778a0288a5fd97c59b5e7f74c1d818146bc82388
Nunarehion <145133685+Nunarehion@users.noreply.github.com>
2025-12-07 01:45:27 +0300

    Update +layout.svelte

--- Svelte/WebShell/src/routes/+layout.svelte
+++ Svelte/WebShell/src/routes/+layout.svelte
@@ -29,6 +29,11 @@
     <hr style="margin: 1rem 0;" />
     <div class="items">
       <LinkItem url="/files" icon="/icon-folder.svg"></LinkItem>
+    <div style="position: fixed; top:0; left:0; color:white; background: black; display: grid; gap: 1rem">
+{#each data.bookmarks as mark}
+   <a href="{mark.url}"> {mark.url.split("/").at(-1)}</a>
+{/each}
+</div>
     </div>
   </SideBar>
 
